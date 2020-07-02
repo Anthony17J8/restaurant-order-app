e1296c5b30a6685544c7a5dfecef5cbe0aab6057
Kiselev <j8kiselev@gmail.com>
2020-07-02 22:36:19 +0300

    Init commit

---
+++ src/main/java/com/x5/restaurantorderapp/RestaurantOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.x5.restaurantorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/x5/restaurantorderapp/RestaurantOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x5.restaurantorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
